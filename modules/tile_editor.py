46a97e0ecd2f227c5f6092d5d4b8401a071ab867
Z <>
2026-03-22 10:32:48 +0800

    tile-editor: click handler

--- modules/tile_editor.py
+++ modules/tile_editor.py
@@ -29,5 +29,19 @@ class TileEditor:
 
             3: (100, 100, 100),
 
-            4: (0, 100, 255)
+            4: (255, 100, 0)
         }
+
+    def handle_click(self, mouse_x, mouse_y, button):
+
+        adjusted_x = mouse_x - self.offset_x
+        adjusted_y = mouse_y - self.offset_y
+
+        col = adjusted_x // self.tile_size
+        row = adjusted_y // self.tile_size
+
+        if 0 <= col < self.width and 0 <= row < self.height:
+            if button == 1:
+                self.map_data[row][col] = self.current_tile
+            elif button == 3:
+                self.map_data[row][col] = 0

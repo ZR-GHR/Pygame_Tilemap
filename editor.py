5db4e79456b732d23ed120ad8facf991998e8f40
Z <>
2026-04-05 10:41:08 +0800

    editor: init

---
+++ editor.py
@@ -0,0 +1,13 @@
+import pygame as pg
+from modules.tile_editor import TileEditor
+
+pg.init()
+screen = pg.display.set_mode((800, 600))
+pg.display.set_caption("Tilemap Editor")
+clock = pg.time.Clock()
+
+editor = TileEditor(width = 20, height = 15, tile_size = 32)
+
+editor.load_from_file('tilemap.json')
+
+mouse_presssed = False
--- test.py
+++
@@ -1,4 +0,0 @@
-age  = -4
-print(f'Hello, Tom, you are -4 years old')
-
-print('New')

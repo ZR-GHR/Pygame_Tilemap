fd39e1e2bac00e44e88802e8a4ae4892c379a973
Z <>
2026-04-12 09:13:58 +0800

    init commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# init
+import pygame as pg
+from modules import player, tilemap
 
+pg.init()
+screen = pg.display.set_mode((800, 600))
+pg.display.set_caption("Tilemap Platformer")
+clock = pg.time.Clock()
